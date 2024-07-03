51f67c4a491e52cef473d9150aef4b5fa4c17608
Gunjankher <gkhercode@gmail.com>
2024-07-03 11:06:55 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+
 
 
 function App() {
@@ -6,7 +9,12 @@ function App() {
 
   return (
     <>
-     <h1>e-commerce app</h1>
+  <div>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
+  </div>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/app.scss'
+import {createBrowserRouter,createRoutesFromElements,Routes,RouterProvider,Route} from 'react-router-dom'
+import { Suspense, lazy } from 'react'
+import Loader from './components/Loader.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>\
+
+
+    <Route 
+    path='/'
+    element ={
+      <Suspense fallback={<Loader />}>
+  
+    </Suspense>
+    }
+    />
+    
+
+
+
+    
+    
+    </>
+  )
+)
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
