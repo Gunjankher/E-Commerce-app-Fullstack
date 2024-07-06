4b784ee3b6bf397b13a81db908f579465c0fd066
Gunjankher <gkhercode@gmail.com>
2024-07-06 15:32:02 +0530

    Backend setup

--- backend/src/utilis/cloudinary.js
+++ backend/src/utilis/cloudinary.js
@@ -1,10 +1,34 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { response } from 'express';
+import fs from 'fs'
 
 
     cloudinary.config({ 
-        cloud_name: 'dyog39lnl', 
-        api_key: '143116211184196', 
-        api_secret: '<your_api_secret>' // Click 'View Credentials' below to copy your API secret
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret:process.env.CLOUDINARY_API_SECRET
     });
     
   
+    const uploadOnCloudinary = async (localFilePath)=>{
+        try {
+            
+if(localFilePath) return null
+
+if(localFilePath){
+     const response = await cloudinary.uploader.upload(localFilePath,{
+        resource_type :'auto'
+    })
+
+    fs.unlinkSync(localFilePath)
+    return response
+}
+
+        } catch (error) {
+            fs.unlinkSync(localFilePath)
+            return null
+        }
+    }
+
+
+    export {uploadOnCloudinary}
