a7664ff774d6b52c6d7816706a67896b109b0cba
Gunjankher <gkhercode@gmail.com>
2024-07-11 10:54:25 +0530

    product

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,13 +1,40 @@
 import { asyncHandler } from "../utilis/asyncHandler.js";
+import { Product } from "../models/products.model.js";
+import { ApiError } from "../utilis/ApiError.js";
+import { ApiResponse } from "../utilis/ApiResponse.js";
+
 
 
 
 
 const newProduct = asyncHandler(async(req,res)=>{
 
+try {
+    const {name,price,stock,category,description} = req.body
+    const photo = req.file;
+    
+    
+    
+     const product =  await Product.create({
+        name,
+        price,
+        stock,
+        category : category.toLowerCase(),
+        photo : photo?.path
+    })
+    
+    
+    if(!product) return new ApiError(400,"Error While Creating Product")
+    return res
+    .status(201)
+    .json(new ApiResponse (200,{},"product Created Sucessfully"))
+} catch (error) {
+    new ApiError(401, error.message , "can not Create Product")
+}
+
 })
 
 
 export {
     newProduct
-}
+} 
