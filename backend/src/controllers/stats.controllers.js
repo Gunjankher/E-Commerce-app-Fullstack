19f8b7ef53e1112d9f330e92deaf28c0854b1003
Gunjankher <gkhercode@gmail.com>
2024-07-25 16:22:10 +0530

    stats

--- backend/src/controllers/stats.controllers.js
+++ backend/src/controllers/stats.controllers.js
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utilis/asyncHandler.js";
 
 
 
-const deleteOrder = asyncHandler(async (req, res, next) => {
+const getDashBoardStats = asyncHandler(async (req, res, next) => {
     try {
      
   
@@ -15,3 +15,50 @@ const deleteOrder = asyncHandler(async (req, res, next) => {
       return next(new ApiError(401, error.message, 'Cannot delete order'));
     }
   });
+
+
+const getPieCharts = asyncHandler(async (req, res, next) => {
+    try {
+     
+  
+      return res.status(200).json(new ApiResponse(200, 'Order deleted successfully'));
+    } catch (error) {
+      console.error('Error deleting order:', error.message);
+      return next(new ApiError(401, error.message, 'Cannot delete order'));
+    }
+  });
+
+
+const getBarCharts = asyncHandler(async (req, res, next) => {
+    try {
+     
+  
+      return res.status(200).json(new ApiResponse(200, 'Order deleted successfully'));
+    } catch (error) {
+      console.error('Error deleting order:', error.message);
+      return next(new ApiError(401, error.message, 'Cannot delete order'));
+    }
+  });
+
+
+const getLineCharts = asyncHandler(async (req, res, next) => {
+    try {
+     
+  
+      return res.status(200).json(new ApiResponse(200, 'Order deleted successfully'));
+    } catch (error) {
+      console.error('Error deleting order:', error.message);
+      return next(new ApiError(401, error.message, 'Cannot delete order'));
+    }
+  });
+
+
+
+
+
+  export {
+    getDashBoardStats,
+    getBarCharts,
+    getLineCharts,
+    getPieCharts,
+  }
--- backend/src/routes/stats.router.js
+++ backend/src/routes/stats.router.js
@@ -1,7 +1,5 @@
 import { adminOnly } from "../middlerware/auth.middleware.js";
-import {singleUpload} from '../middlerware/multer.middleware.js'
-import { asyncHandler } from "../utilis/asyncHandler.js";
-import { allOrders, deleteOrder, getSingleOrder, myOrders, newOrder, processOrder } from "../controllers/order.controllers.js";
+
 
 
 
