50bd33b7878d1f7e65336518034789b96528f12d
Gunjankher <gkhercode@gmail.com>
2024-07-27 14:12:16 +0530

    stats

--- backend/src/controllers/stats.controllers.js
+++ backend/src/controllers/stats.controllers.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utilis/asyncHandler.js";
 import {myCache} from '../app.js'
 import { ApiError } from "../utilis/ApiError.js";
 import { ApiResponse } from "../utilis/ApiResponse.js";
+import { Product } from "../models/products.model.js";
 
 
 
@@ -16,6 +17,67 @@ let stats;
 if(myCache.has("admin-stats")) 
   stats = JSON.parse(myCache.get("admin-stats"))
 else{
+
+  const today = new Date()
+
+  const thisMonth = {
+    start: new Date(today.getFullYear(), today.getMonth(), 1),
+    end: today,
+  };
+
+  const lastMonth = {
+    start: new Date(today.getFullYear(), today.getMonth() - 1, 1),
+    end: new Date(today.getFullYear(), today.getMonth(), 0),
+  };
+  // const startofThisMonth = new Date(today.getFullYear(),today.getMonth(),1)
+  // const startofLastMonth = new Date(today.getFullYear(),today.getMonth(),-1)
+  // const endofLastMonth = new Date(today.getFullYear(),today.getMonth(),0)
+
+
+  const thisMonthProductsPromise = Product.find({
+    createdAt: {
+      $gte: thisMonth.start,
+      $lte: thisMonth.end,
+    },
+  });
+
+  const lastMonthProductsPromise = Product.find({
+    createdAt: {
+      $gte: lastMonth.start,
+      $lte: lastMonth.end,
+    },
+  });
+
+  const thisMonthUsersPromise = User.find({
+    createdAt: {
+      $gte: thisMonth.start,
+      $lte: thisMonth.end,
+    },
+  });
+
+  const lastMonthUsersPromise = User.find({
+    createdAt: {
+      $gte: lastMonth.start,
+      $lte: lastMonth.end,
+    },
+  });
+
+  const thisMonthOrdersPromise = Order.find({
+    createdAt: {
+      $gte: thisMonth.start,
+      $lte: thisMonth.end,
+    },
+  });
+
+  const lastMonthOrdersPromise = Order.find({
+    createdAt: {
+      $gte: lastMonth.start,
+      $lte: lastMonth.end,
+    },
+  });
+
+
+
 
 }
 
@@ -57,13 +119,6 @@ const getBarCharts = asyncHandler(async (req, res, next) => {
 const getLineCharts = asyncHandler(async (req, res, next) => {
     try {
      
-  const today = new Date()
-  const startofThisMonth = new Date(today.getFullYear(),today.getMonth(),1)
-  const startofLastMonth = new Date(today.getFullYear(),today.getMonth(),-1)
-
-
-
-
       return res.status(200).json(new ApiResponse(200, 'Order deleted successfully'));
     } catch (error) {
       console.error('Error deleting order:', error.message);
