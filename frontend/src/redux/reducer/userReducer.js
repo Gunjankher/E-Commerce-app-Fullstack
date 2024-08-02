b41897693e015d17919b141f7908397d6f4bbf7f
Gunjankher <gkhercode@gmail.com>
2024-08-02 10:43:10 +0530

    git ignore

---
+++ frontend/src/redux/reducer/userReducer.js
@@ -0,0 +1,15 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+
+const initalState = {}
+
+export const userReducer  = createSlice({
+name :"userReducer",
+initalState,
+reducers :{
+
+}
+
+
+})
