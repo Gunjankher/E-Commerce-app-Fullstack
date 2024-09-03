baed87b05a87c783bcc88613f2b0e222f0399d52
Gunjankher <gkhercode@gmail.com>
2024-09-03 10:27:44 +0530

    debugging

--- frontend/src/Pages/Admin-Pages/Products.jsx
+++ frontend/src/Pages/Admin-Pages/Products.jsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
 import { useAllProductsQuery } from '../../redux/api/productApi';
 import { useSelector } from 'react-redux';
-import { Skeleton } from '../../components/Loader';
 import { server } from '../../redux/store';
+import toast from 'react-hot-toast';
 
 
 const columns = [
@@ -34,131 +34,96 @@ const columns = [
   },
 ];
 
-// const img =
-//   "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
 
-// const img2 = "https://m.media-amazon.com/images/I/514T0SvwkHL._SL1500_.jpg";
-
-// const arr = [
-//   {
-//     photo: <img src={img} alt="Shoes" />,
-//     name: "Puma Shoes Air Jordan Cook Nigga 2023",
-//     price: 690,
-//     stock: 3,
-//     action: <Link to="/admin/product/sajknaskd">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img2} alt="Shoes" />,
-//     name: "Macbook",
-//     price: 232223,
-//     stock: 213,
-//     action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img} alt="Shoes" />,
-//     name: "Puma Shoes Air Jordan Cook Nigga 2023",
-//     price: 690,
-//     stock: 3,
-//     action: <Link to="/admin/product/sajknaskd">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img2} alt="Shoes" />,
-//     name: "Macbook",
-//     price: 232223,
-//     stock: 213,
-//     action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img} alt="Shoes" />,
-//     name: "Puma Shoes Air Jordan Cook Nigga 2023",
-//     price: 690,
-//     stock: 3,
-//     action: <Link to="/admin/product/sajknaskd">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img2} alt="Shoes" />,
-//     name: "Macbook",
-//     price: 232223,
-//     stock: 213,
-//     action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
-//   },
-//   {
-//     photo: <img src={img2} alt="Shoes" />,
-//     name: "Macbook",
-//     price: 232223,
-//     stock: 213,
-//     action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
-//   },
-// ];
 
 const Products = () => {
-
-  const {user} = useSelector((state)=>state.userReducer)
-  const {data,isLoading,error} = useAllProductsQuery(user?._id)
-  
-
   
   const [rows,setRows] = useState([]);
-
-
-// useEffect(()=>{
-//   if (data&& data.data)
-//     setRows(
-//       data?.data.map((i) => ({
-//         photo: <img src={`${server}/${i.photo}`} />,
-//         name: i.name,
-//         price: i.price,
-//         stock: i.stock,
-//        action: <Link to={`/admin/product/${i._id}`}>Manage</Link>,
-//        }))
-//     );
-// },[data])
-
-
-
-
-
-useEffect(() => {
-    if (data?.data && data?.data.length > 0) {
-      setRows(
-        data?.data?.map((i) => ({
-          photo: (
-            <img
-              src={`${server}/${i.photos[0]?.url}`}
-             
-            />
-          ),
-          name: i.name,
-          price: i.price,
-          stock: i.stock,
-          action: <Link to={`/admin/product/${i._id}`}>Manage</Link>,
-        }))
-      );
-    } 
+  const {user} = useSelector((state)=>state.userReducer)
+  const {data, isError,} = useAllProductsQuery(user?._id)
   
-}, [data]);
+  if (isError) {
+    toast.error('Some error occured')
+  }
 
 
-// if (isLoading) return <div>Loading...</div>;
-// if (error) return <div>Error loading products</div>;
+  // useEffect(() => {
+  //   if (data?.data && data.data.length > 0) {
+  //     setRows(
+  //       data?.data?.map((i) => ({
+  //         photo: <img src={`${server}/${i.photos[0]?.url}`} alt={i.name} />,
+  //         name: i.name,
+  //         price: i.price,
+  //         stock: i.stock,
+  //         action: <Link to={`/admin/product/${i._id}`}>Manage</Link>,
+  //       }))
+  //     )
+  //   } else {
+  //    console.log(`fk yaar`);
+     
+  //   }
+  // }, [data]);
 
 
-console.log(`data products` , data);
+
+  const mapDataToRows = (data) => {
+    return data?.map((i) => ({
+      photo: <img src={`${server}/${i.photos[0]?.url}`} alt={i.name} />,
+      name: i.name,
+      price: i.price,
+      stock: i.stock,
+      action: <Link to={`/admin/product/${i._id}`}>Manage</Link>,
+    }));
+  };
+
+  useEffect(() => {
+    if (data?.data && data.data.length > 0) {
+      const mappedRows = mapDataToRows(data.data);
+      setRows(mappedRows);
+    } else {
+      console.log('No products found');
+    }
+  }, [data]);
+
+  
+  
+  
+  
+  
+  
+  // console.log(`Array Data`,Array.isArray(data));// log says flase
+// console.log(`Array Data`,Array.isArray(rows));  // log says true
+
+// console.log(`type of data`, typeof data); // log says object
+// console.log(`type of data`, typeof rows); // log says object
+
+
+// console.log(`data products`,data)
 
   const Table = useCallback(
     TableHOC(columns, rows, "dashboard-product-box", "Products", true),
     []
   );
 
+  
+  
   return (
     <div className="adminContainer">
-      <AdminSideBar />
-      <main>{isLoading ? <Skeleton length={20}/>:<Table />}</main>
-      <Link to="/admin/product/new" className="create-product-btn">
-        <FaPlus />
-      </Link>
-    </div>
+    <AdminSideBar />
+    <main>{ <Table />}</main>
+    <Link to="/admin/product/new" className="create-product-btn">
+      <FaPlus />
+    </Link>
+  </div>
   );
 };
 
 export default Products;
+
+
+
+// photo: <img src={`${server}/${i.photo}`}/>,
+// name: i.name,
+// price: i.price,
+// stock: i.stock,
+// action: <Link to={`/admin/product/${i._id}`}>Manage</Link>,
