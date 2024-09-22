bebf73170f470671bab856857c49e78d1e75318d
Gunjankher <gkhercode@gmail.com>
2024-09-22 18:46:17 +0530

    cart

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { productAPI } from './api/productApi'
 import { userAPI } from './api/userApi'
 import { userReducer } from './reducer/userReducer'
+import { cartReducer } from './reducer/cartReducer'
 
 
 
@@ -13,7 +14,8 @@ export const store = configureStore({
     reducer :{
     [userAPI.reducerPath] : userAPI.reducer,
     [productAPI.reducerPath] : productAPI.reducer,
-    [userReducer.name] :userReducer.reducer
+    [userReducer.name] :userReducer.reducer,
+    [cartReducer.name] :cartReducer.reducer
     },
     middleware :(mid)=>[...mid(),userAPI.middleware,productAPI.middleware]
 })
